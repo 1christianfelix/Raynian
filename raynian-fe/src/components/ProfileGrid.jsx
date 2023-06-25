8ca4081a674172a01a34ce819b23f8b6ba3bfaf0
Christian Felix <1christianfelix@gmail.com>
2023-06-25 02:21:51 -0700

    fixed profile grid

--- raynian-fe/src/components/ProfileGrid.jsx
+++ raynian-fe/src/components/ProfileGrid.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Profile from "./Profile";
-const ProfileGrid = ({ profiles }) => {
-  let gridCols = profiles.length; // Number of columns in the grid
-  if (gridCols < 1) {
-    gridCols = 1; // Minimum of 1 column
-  } else if (gridCols > 3) {
-    gridCols = 3; // Maximum of 3 columns
-  }
 
-  // Rethink grid size and profile picture size as the more people join.
+const ProfileGrid = ({ profiles }) => {
+  const [gridCols, setGridCols] = useState("");
+
+  useEffect(() => {
+    let cols = profiles.length;
+
+    if (cols < 1) {
+      cols = 1; // Minimum of 1 column
+    } else if (cols > 3) {
+      cols = 3; // Maximum of 3 columns
+    }
+
+    setGridCols(cols);
+  }, [profiles]);
+
+  useEffect(() => {
+    setGridCols(`grid-cols-${profiles.length}`); // Trigger state update after initial rendering
+  }, []);
 
   return (
-    <div className={`grid grid-cols-${gridCols} gap-4 pt-10`}>
-      {profiles.map((profile, index) => (
-        <Profile key={index} pfp={profile} />
-      ))}
-    </div>
+    <>
+      {gridCols && (
+        <div className={`grid ${gridCols} gap-4 pt-10`}>
+          {profiles.map((profile, index) => (
+            <Profile key={index} pfp={profile} />
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
