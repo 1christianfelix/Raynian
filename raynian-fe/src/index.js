248382775af8b33072a432cdf296d117ee2ec7f3
Christian Felix <1christianfelix@gmail.com>
2023-06-29 01:47:22 -0700

    Refactored app.js code into dashboard component and set up routes for user auth

--- raynian-fe/src/index.js
+++ raynian-fe/src/index.js
@@ -1,13 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+function Root() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
