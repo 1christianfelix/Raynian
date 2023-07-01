f9edde3d55f80e148462fa07c5788ff2e689a18d
Abram Felix <abramfelix1@gmail.com>
2023-07-01 16:35:17 -0700

    refactored userDropdown and separate dropdown from modals

--- raynian-fe/src/components/util/TimerSettings.jsx
+++ raynian-fe/src/components/util/TimerSettings.jsx
@@ -1,18 +1,36 @@
 import React from "react";
-import { useContext, useState } from "react";
+import { useContext, useState, useRef, useEffect } from "react";
 import { TimerContext } from "../../context/TimerContext";
 
-export default function TimerSettings() {
+export default function TimerSettings(props) {
   const [workOption, setWorkOption] = useState("default");
   const [breakOption, setBreakOption] = useState("default");
   const [backgroundOption, setBackgroundOption] = useState("default");
+  const dropdownRef = useRef(null);
 
-  const handleChange = (e) => {
-    setWorkOption(e.target.value);
+  const { setOpenSettings, settingsRef } = props;
+
+  const handleClickOutside = (event) => {
+    if (settingsRef.current && settingsRef.current.contains(event.target)) {
+      return;
+    }
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setOpenSettings(false);
+    }
   };
 
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, []);
+
   return (
-    <div className="absolute top-[7px] left-[65px] bg-white drop-shadow-md rounded-md text-sm py-[15px] px-[20px]">
+    <div
+      className="absolute top-[7px] left-[65px] bg-white drop-shadow-md rounded-md text-sm py-[15px] px-[20px]"
+      ref={dropdownRef}
+    >
       <div className="flex justify-between mb-[10px]">
         <p className="mr-[10px] whitespace-nowrap">Work Time</p>
         <div>
@@ -22,9 +40,9 @@ export default function TimerSettings() {
               setWorkOption(e.target.value);
             }}
           >
-            <option value="default">default</option>
-            <option value="option1">option1</option>
-            <option value="option2">option2</option>
+            <option value="60 min">60 min</option>
+            <option value="45 min">45 min</option>
+            <option value="30 min">30 min</option>
           </select>
         </div>
       </div>
@@ -36,9 +54,9 @@ export default function TimerSettings() {
             setBreakOption(e.target.value);
           }}
         >
-          <option value="default">default</option>
-          <option value="option1">option1</option>
-          <option value="option2">option2</option>
+          <option value="15 min">15 min</option>
+          <option value="10 min">10 min</option>
+          <option value="5 min">5 min</option>
         </select>
       </div>
       <div className="flex justify-between">
