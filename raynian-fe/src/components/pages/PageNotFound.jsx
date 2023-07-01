0276accfc225bfb40daa326ae75dd014533b38bc
Ahljenn <aamallari@ucdavis.edu>
2023-07-01 15:17:49 -0700

    changed wolf sprite

--- raynian-fe/src/components/pages/PageNotFound.jsx
+++ raynian-fe/src/components/pages/PageNotFound.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import avocado from '../../assets/temp_pfp/avocado.jpg';
+import wolf from '../../assets/temp_pfp/wolf.png';
 
 export const PageNotFound = () => {
   return (
@@ -8,7 +8,7 @@ export const PageNotFound = () => {
         <h1 className="mt-10">404</h1>
         <p className="mt-10">How did this happen?</p>
 
-        <img className="mt-[5rem]" src={avocado} alt="Error sprite" height={184} width={184} />
+        <img className="mt-[5rem]" src={wolf} alt="Error sprite" height={184} width={184} />
       </div>
     </>
   );
