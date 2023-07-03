44ed95eef178d48a820f00b271375d3ae64b8860
Abram Felix <abramfelix1@gmail.com>
2023-07-03 16:06:57 -0700

    added tooltips to function buttons

--- raynian-fe/src/components/util/Timer.jsx
+++ raynian-fe/src/components/util/Timer.jsx
@@ -5,12 +5,20 @@ import {
   IoPlayOutline,
   IoStopOutline,
   IoSettingsOutline,
+  IoPlaySkipForwardOutline,
 } from "react-icons/io5";
 import { PiPauseLight } from "react-icons/pi";
 import TimerSettings from "./TimerSettings";
+import "../../index.css";
+import { motion } from "framer-motion";
 
 export default function Timer() {
   const [openSettings, setOpenSettings] = useState(false);
+  const [playHovered, setPlayHovered] = useState(false);
+  const [pauseHovered, setPauseHovered] = useState(false);
+  const [stopHovered, setStopHovered] = useState(false);
+  const [skipHovered, setSkipHovered] = useState(false);
+  const [settingsHovered, setSettingsHovered] = useState(false);
   const settingsRef = useRef();
   const {
     countdown,
@@ -52,13 +60,33 @@ export default function Timer() {
         <div className="flex flex-col justify-center absolute bottom-[35px] right-[-15px]">
           {!isRunning && (
             <div
-              className="mx-[5px] my-[5px] absolute bottom-[58px]"
+              className="mx-[5px] my-[5px] absolute bottom-[58px] flex flex-row"
               onClick={startTimer}
             >
               <IoPlayOutline
                 size={22}
                 className="timer-button text-green-700 cursor-pointer"
+                onMouseEnter={() => setPlayHovered(true)}
+                onMouseLeave={() => setPlayHovered(false)}
               />
+              {playHovered && (
+                <motion.div
+                  className="tooltip absolute left-[30px] top-[-1px]"
+                  initial={{ width: 0 }}
+                  animate={{ width: "auto" }}
+                  transition={{ duration: 0.05 }}
+                  style={{ pointerEvents: "none" }}
+                >
+                  <motion.p
+                    className="whitespace-nowrap text-white text-xs align"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                  >
+                    Start Timer
+                  </motion.p>
+                </motion.div>
+              )}
             </div>
           )}
           {isRunning && (
@@ -69,24 +97,114 @@ export default function Timer() {
               <PiPauseLight
                 size={20}
                 className="timer-button text-slate-700 cursor-pointer dark:text-white"
+                onMouseEnter={() => setPauseHovered(true)}
+                onMouseLeave={() => setPauseHovered(false)}
               />
+              {pauseHovered && (
+                <motion.div
+                  className="tooltip absolute left-[30px] top-[-3px]"
+                  initial={{ width: 0 }}
+                  animate={{ width: "auto" }}
+                  transition={{ duration: 0.05 }}
+                  style={{ pointerEvents: "none" }}
+                >
+                  <motion.p
+                    className="whitespace-nowrap text-white text-xs align"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                  >
+                    Pause Timer
+                  </motion.p>
+                </motion.div>
+              )}
             </div>
           )}
-          <div className="mx-[5px] my-[5px]" onClick={stopTimer}>
-            <IoStopOutline
-              size={20}
-              className="timer-button text-red-700 cursor-pointer"
-            />
-          </div>
+          {isWork && (
+            <div className="mx-[5px] my-[5px]" onClick={stopTimer}>
+              <IoStopOutline
+                size={20}
+                className="timer-button text-red-700 cursor-pointer"
+                onMouseEnter={() => setStopHovered(true)}
+                onMouseLeave={() => setStopHovered(false)}
+              />
+              {stopHovered && (
+                <motion.div
+                  className="tooltip absolute left-[35px] top-[2px]"
+                  initial={{ width: 0 }}
+                  animate={{ width: "auto" }}
+                  transition={{ duration: 0.05 }}
+                  style={{ pointerEvents: "none" }}
+                >
+                  <motion.p
+                    className="whitespace-nowrap text-white text-xs align"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                  >
+                    Stop Timer
+                  </motion.p>
+                </motion.div>
+              )}
+            </div>
+          )}
+          {isBreak && (
+            <div className="mx-[5px] my-[5px]" onClick={stopTimer}>
+              <IoPlaySkipForwardOutline
+                size={20}
+                className="timer-button text-slate-700 cursor-pointer dark:text-white"
+                onMouseEnter={() => setSkipHovered(true)}
+                onMouseLeave={() => setSkipHovered(false)}
+              />
+              {skipHovered && (
+                <motion.div
+                  className="tooltip absolute left-[35px] top-[2px]"
+                  initial={{ width: 0 }}
+                  animate={{ width: "auto" }}
+                  transition={{ duration: 0.05 }}
+                  style={{ pointerEvents: "none" }}
+                >
+                  <motion.p
+                    className="whitespace-nowrap text-white text-xs align"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                  >
+                    Skip Break
+                  </motion.p>
+                </motion.div>
+              )}
+            </div>
+          )}
           <div
-            className="mx-[5px] my-[5px]"
+            className="mx-[5px] my-[5px] relative"
             onClick={toggleSettings}
             ref={settingsRef}
           >
             <IoSettingsOutline
               size={20}
               className="timer-button text-slate-700 cursor-pointer dark:text-white"
+              onMouseEnter={() => setSettingsHovered(true)}
+              onMouseLeave={() => setSettingsHovered(false)}
             />
+            {settingsHovered && (
+              <motion.div
+                className="tooltip absolute left-[30px] top-[-2px]"
+                initial={{ width: 0 }}
+                animate={{ width: "auto" }}
+                transition={{ duration: 0.05 }}
+                style={{ pointerEvents: "none" }}
+              >
+                <motion.p
+                  className="whitespace-nowrap text-white text-xs align"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                >
+                  Settings
+                </motion.p>
+              </motion.div>
+            )}
           </div>
           {openSettings && (
             <TimerSettings
@@ -98,7 +216,7 @@ export default function Timer() {
         </div>
       </div>
 
-      <p className="text-3xl">
+      <p className="text-3xl select-none">
         Sessions completed today:<span className="text-3xl"> 2</span>
       </p>
     </>
