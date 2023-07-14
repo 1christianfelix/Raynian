d896b5916f1a8a27cf9fe620a1f67e262e16eb43
Christian Felix <1christianfelix@gmail.com>
2023-07-13 20:04:36 -0700

    removed test components

--- raynian-fe/src/App.js
+++ raynian-fe/src/App.js
@@ -14,9 +14,6 @@ import ColorSelection from "./components/ui/background-customizer/ColorSelection
 
 import Dashboard from "./components/pages/Dashboard";
 import { PageNotFound } from "./components/pages/PageNotFound";
-import GradientSlider from "./components/ui/background-customizer/GradientSlider";
-import GradientSliderHandle from "./components/ui/background-customizer/GradientSliderHandle";
-import BgCustomizerMenu from "./components/ui/background-customizer/BgCustomizerMenu";
 import { BGCustomContext } from "./context/BGCustomContext";
 
 function App() {
@@ -28,10 +25,6 @@ function App() {
           className="h-screen w-screen bg-gradient-to-b from-slate-100 to-rose-100 font-thin dark:bg-gradient-to-b dark:from-slate-700 dark:to-slate-800 dark:text-white"
           style={{ background: bg }}
         >
-          {/* <ColorSelection></ColorSelection> */}
-          {/* <BgCustomizerMenu></BgCustomizerMenu> */}
-          {/* <GradientSlider></GradientSlider> */}
-          {/* <GradientSliderHandle></GradientSliderHandle> */}
           <Modal />
           <Nav />
           <Routes>
