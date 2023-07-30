0ee88a24843ebcdfef0540235b41f2071c64b435
Christian Felix <1christianfelix@gmail.com>
2023-07-30 16:45:46 -0700

    RoomButton lint

--- raynian-fe/src/components/rooms/RoomButton.jsx
+++ raynian-fe/src/components/rooms/RoomButton.jsx
@@ -1,42 +1,9 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { ModalContext } from "../../context/ModalContext";
-import { socketServerConnect } from "../socket/socketConnection";
-import { useDispatch } from "react-redux";
-import { connectToRoom, setUserInfo } from "../../slices/roomSlice";
 
 const RoomButton = () => {
-  const [user, setUser] = useState({ username: "", roomId: null });
   const { toggleRoomPrompt } = useContext(ModalContext);
 
-  const dispatch = useDispatch();
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUser((prevUser) => ({ ...prevUser, [name]: value }));
-  };
-
-  const handleSubmit = () => {
-    // You can perform any additional logic here if needed before updating the state.
-    // For now, we are simply updating the state with the entered username.
-    // You may want to add validation or other checks depending on your use case.
-    if (user.username.length > 1 && user.roomId != null) {
-      console.log("test");
-      socketServerConnect(user);
-      handleJoinRoom();
-    }
-  };
-
-  const handleJoinRoom = () => {
-    const { username, roomId } = user;
-
-    if (username.trim() === "" || roomId.trim() === "") {
-      return;
-    }
-
-    // Dispatch the actions to update the roomID and add the participant
-    dispatch(setUserInfo(username));
-    dispatch(connectToRoom(roomId));
-  };
   return (
     <div className="mt-4">
       <button
