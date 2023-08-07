6c5e5bd639c7eba15aa0f162be8394a0a4779269
Christian Felix <1christianfelix@gmail.com>
2023-08-06 23:38:49 -0700

    Display participants mapped

--- raynian-fe/src/components/rooms/ParticipantList.jsx
+++ raynian-fe/src/components/rooms/ParticipantList.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ParticipantList = () => {
-  return <div>ParticipantList</div>;
+  const { participants } = useSelector((state) => state.room);
+  return (
+    <div className="flex flex-col gap-2">
+      {participants.map((participant) => (
+        <div>{participant.username}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ParticipantList;
