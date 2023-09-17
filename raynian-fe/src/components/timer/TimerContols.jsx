3011d1da2a7ac1cb37b0ae33701b41c1d4c911c9
Christian Felix <1christianfelix@gmail.com>
2023-09-16 18:45:17 -0700

    clean up

--- raynian-fe/src/components/timer/TimerContols.jsx
+++ raynian-fe/src/components/timer/TimerContols.jsx
@@ -13,36 +13,16 @@ import {
 } from "react-icons/io5";
 import { PiPauseLight } from "react-icons/pi";
 import { motion, AnimatePresence } from "framer-motion";
-import { Tooltip } from "react-tooltip";
 import TimerTooltip from "./TimerTooltip";
 
 const TimerContols = () => {
   const dispatch = useDispatch();
   const timerState = useSelector((state) => state.timer);
   const { roomId } = useSelector((state) => state.room);
-
-  const [playHovered, setPlayHovered] = useState(false);
-  const [pauseHovered, setPauseHovered] = useState(false);
-  const [stopHovered, setStopHovered] = useState(false);
-  const [skipHovered, setSkipHovered] = useState(false);
   const skipRef = useRef(null);
   const stopRef = useRef(null);
   const pauseRef = useRef(null);
   const startRef = useRef(null);
-
-  const tooltipAnimation = {
-    initial: { width: 0, opacity: 0 },
-    animate: { width: "auto", opacity: 1 },
-    exit: { opacity: 0, width: 0, transition: { duration: 0.05 } },
-    transition: { duration: 0.05 },
-    style: { pointerEvents: "none" },
-  };
-
-  const tooltipTextAnimation = {
-    initial: { opacity: 0 },
-    animate: { opacity: 1 },
-    exit: { opacity: 0, transition: { duration: 0 } },
-  };
 
   const handleStartTimer = () => {
     dispatch(timerActions.startTimer());
@@ -61,9 +41,6 @@ const TimerContols = () => {
 
   return (
     <div className="flex flex-col items-start gap-2">
-      {/* <button onClick={handleStartTimer}>StartTimer</button>
-      <button onClick={handleStopTimer}>StopTimer</button>
-      <button onClick={handlePauseTimer}>PauseTimer</button> */}
       {!timerState.isRunning && (
         <div
           className="group relative flex items-center justify-center gap-2"
@@ -131,52 +108,6 @@ const TimerContols = () => {
           <TimerTooltip type={"skip"} container={skipRef} text={"Skip Break"} />
         </div>
       )}
-
-      {/* <Tooltip
-        id="start"
-        noArrow
-        style={{
-          backgroundColor: "#2cb150",
-          padding: "4px",
-          fontSize: "16px",
-        }}
-        delayShow={100}
-        delayHide={100}
-        middlewares={[]}
-      />
-      <Tooltip
-        id="stop"
-        noArrow
-        style={{
-          backgroundColor: "#c03333",
-          padding: "4px",
-          fontSize: "16px",
-        }}
-        delayShow={100}
-        delayHide={100}
-      />
-      <Tooltip
-        id="skip"
-        noArrow
-        style={{
-          backgroundColor: "#ecbb21",
-          padding: "4px",
-          fontSize: "16px",
-        }}
-        delayShow={100}
-        delayHide={100}
-      />
-      <Tooltip
-        id="pause"
-        noArrow
-        style={{
-          backgroundColor: "#3b73ed",
-          padding: "4px",
-          fontSize: "16px",
-        }}
-        delayShow={100}
-        delayHide={100}
-      /> */}
     </div>
   );
 };
