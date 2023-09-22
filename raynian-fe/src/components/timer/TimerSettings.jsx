242658b63987b32aa0b3d120b461f7f85929d92c
Christian Felix <1christianfelix@gmail.com>
2023-09-22 02:09:18 -0700

    break timer input added

--- raynian-fe/src/components/timer/TimerSettings.jsx
+++ raynian-fe/src/components/timer/TimerSettings.jsx
@@ -14,6 +14,10 @@ const TimerSettings = () => {
     timerState.workTime.minutes
   );
 
+  const [breakTimerInput, setBreakTimerInput] = useState(
+    timerState.breakTime.minutes
+  );
+
   const handleWorkTimeChange = (e) => {
     const newValue = parseInt(e.target.value, 10);
 
@@ -33,20 +37,29 @@ const TimerSettings = () => {
   }, [workTimerInput]);
 
   const handleBreakTimeChange = (e) => {
+    const newValue = parseInt(e.target.value, 10);
+
+    // Check if the input is empty or a valid integer within the desired range
+    if (e.target.value === "" || (newValue >= 0 && newValue <= 999)) {
+      setBreakTimerInput(newValue);
+    }
+  };
+
+  useEffect(() => {
     let timer = {
       hours: 0,
-      minutes: parseInt(e.target.value),
+      minutes: breakTimerInput,
       seconds: 0,
     };
     dispatch(timerActions.setBreakTime(timer));
-  };
+  }, [breakTimerInput]);
 
   return (
     <div className="flex w-[450px] flex-col rounded-3xl px-[30px] py-10">
-      <div className="flex flex-col">
-        <div className="flex flex-col justify-between">
-          <p>Work Time:</p>
-          <div className="w-32 border border-transparent focus-within:border-blue-500">
+      <div className="flex flex-row justify-around">
+        <div className="flex flex-col justify-between font-normal">
+          <p className="text-xs ">Work Time:</p>
+          <div className="w-32 border-b border-neutral-300 focus-within:border-blue-500 focus-within:border">
             <div className="flex flex-row w-full">
               <button
                 className="w-20 hover:bg-black/5"
@@ -79,46 +92,39 @@ const TimerSettings = () => {
           </div>
         </div>
 
-        <div class="custom-number-input h-10 w-32">
-          <label
-            for="custom-input-number"
-            class="w-full text-gray-700 text-sm font-semibold"
-          >
-            Counter Input
-          </label>
-          <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
-            <button
-              data-action="decrement"
-              class=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
-            >
-              <span class="m-auto text-2xl font-thin">−</span>
-            </button>
-            <input
-              type="number"
-              class="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default  text-gray-700  outline-none"
-              name="custom-input-number"
-              value="0"
-            ></input>
-            {/* <button
-              data-action="increment"
-              class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
-            >
-              <span class="m-auto text-2xl font-thin">+</span>
-            </button> */}
+        <div className="flex flex-col justify-between font-normal">
+          <p className="text-xs ">Break Time:</p>
+          <div className="w-32 border-b border-neutral-300 focus-within:border-blue-500 focus-within:border">
+            <div className="flex flex-row w-full">
+              <button
+                className="w-20 hover:bg-black/5"
+                onClick={() => {
+                  setBreakTimerInput((prev) => {
+                    return prev - 1;
+                  });
+                }}
+              >
+                -
+              </button>
+              <input
+                className="w-full text-center outline-none bg-transparent"
+                id="breakTime"
+                type="number" // Set the input type to number
+                value={breakTimerInput}
+                onChange={handleBreakTimeChange}
+              />
+              <button
+                className="w-20 hover:bg-black/5"
+                onClick={() => {
+                  setBreakTimerInput((prev) => {
+                    return prev + 1;
+                  });
+                }}
+              >
+                +
+              </button>
+            </div>
           </div>
-        </div>
-
-        <div className="flex justify-between">
-          <p>Break Time:</p>
-          <select
-            id="breakTime"
-            value={timerState.breakTime}
-            onChange={handleBreakTimeChange}
-          >
-            <option value={15}>15 minutes</option>
-            <option value={10}>10 minutes</option>
-            <option value={5}>5 minutes</option>
-          </select>
         </div>
       </div>
     </div>
